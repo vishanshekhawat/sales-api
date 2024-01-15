0c439322e67e39f4cc2bc60df4c467a3a12595af
vishnu <vishnsingh007@gmail.com>
2024-01-15 18:17:00 +0530

    commit 3

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -11,6 +11,8 @@ import (
 	"go.uber.org/zap"
 )
 
+var build = "develop"
+
 func main() {
 
 	log, err := logger.New("SALES-API")
@@ -32,7 +34,7 @@ func run(log *zap.SugaredLogger) error {
 	// -------------------------------------------------------------------------
 	// GOMAXPROCS
 
-	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "BUILD", build)
 
 	// -------------------------------------------------------------------------
 
